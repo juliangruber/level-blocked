bf1b2f6f3fc794f51acf759ee317a83e39197faa
Julian Gruber <julian@juliangruber.com>
2013-10-24 10:10:01 +0200

    small refactor + fix node 0.8 oob bug

--- index.js
+++ index.js
@@ -20,22 +20,26 @@ Blocked.prototype.createReadStream = function(key, opts) {
   var start = opts.start || 0;
   var endSet = typeof opts.end != 'undefined';
 
-  var startIdx = start
-    ? Math.floor(start / this.blockSize)
-    : 0;
-  var endIdx = endSet && opts.end > 0
-    ? Math.floor(opts.end / this.blockSize)
-    : -1;
+  var startBlock = {
+    idx: start
+      ? Math.floor(start / this.blockSize)
+      : 0,
+    start: start
+      ? start % this.blockSize
+      : 0
+  };
 
-  var blockStart = start
-    ? start % this.blockSize
-    : 0;
-  var blockEnd = endSet && opts.end % this.blockSize;
+  var endBlock = {
+    idx: endSet && opts.end > 0
+      ? Math.floor(opts.end / this.blockSize)
+      : -1,
+    end: endSet && opts.end % this.blockSize
+  };
 
-  var idx = startIdx;
+  var idx = startBlock.idx;
 
   rs._read = function() {
-    if (endSet && idx > endIdx) return rs.push(null);
+    if (endSet && idx > endBlock.idx) return rs.push(null);
 
     db.get(join(key, 'blocks', idx), function(err, block) {
       if (err) {
@@ -46,11 +50,11 @@ Blocked.prototype.createReadStream = function(key, opts) {
         return;
       }
 
-      if (start != 0 && idx == startIdx) {
-        block = block.slice(blockStart);
+      if (start != 0 && idx == startBlock.idx) {
+        block = block.slice(startBlock.start);
       }
-      if (endSet && idx == endIdx) {
-        block = block.slice(0, blockEnd + 1);
+      if (endSet && idx == endBlock.idx) {
+        block = block.slice(0, Math.min(block.length, endBlock.end + 1));
       }
       if (!block.length) return rs.push(null);
 
